1539c6dc61f983c799e6d2d5086cb6df888f8e81
Duilio Pastorelli <mezgec.danilo@gmail.com>
2015-08-02 17:16:32 +0100

    Added the settings.container Is now possible to call multiple scripts with different classes or ids.

--- js/script.js
+++ js/script.js
@@ -8,24 +8,25 @@ $(function( $ ) {
 
 	//Set the default options
 	$.fn.jqStarRanking = function( options ){
-		var settings = $.extend({
+		var $settings = $.extend({
+			container: ".jq-star-container",
 			starNumber: "5",
-			fontSize: "24px",
+			size: "24px",
 			color: "yellow"
 		}, options );
 
 		//Create the stars by the user or default settings
-		for (var i = 0; i < settings.starNumber; i++) {
+		for (var i = 0; i < $settings.starNumber; i++) {
 			$("<span>",{
 				class: "jq-star-ranking-empty"
 			}).css({
-				"font-size": settings.fontSize,
-				color: settings.color
-			}).appendTo(".jq-star-container");
+				"font-size": $settings.size,
+				color: $settings.color
+			}).appendTo( $settings.container );
 		}
 
 		//Change the star icon on mouse over, either the stars previous the selected one
-		var $star = $(".jq-star-container").find("span");
+		var $star = $( $settings.container).find("span");
 		$($star).mouseenter(function () {
 			$(this).addClass("jq-star-ranking-full").removeClass("jq-star-ranking-empty");
 			var $n = $star.index(this);
@@ -42,16 +43,16 @@ $(function( $ ) {
 
 		//Set the container's data attribute
 		$star.click(function () {
-			$(".jq-star-container").data("active", ($star.index(this) + 1));
-			console.log("You voted " + $(".jq-star-container").data("active")); //dev
+			$( $settings.container).data("active", ($star.index(this) + 1));
+			console.log("You voted " + $( $settings.container).data("active")); //dev
 		})
 
 		//Check the data->active value on mouse leave and set the stars' icon consequently
 		$star.mouseleave(function () {
-			if (($(".jq-star-container").data("active")) > 0) {
+			if (($( $settings.container).data("active")) > 0) {
 				for (var i = 0; i < $star.length; i++) {	//based on the attribute "active", it made the memory effect on the stars
 					var $element = $star.toArray()[i];
-						if ((i + 1) <= ($(".jq-star-container").data("active"))) {
+						if ((i + 1) <= ($( $settings.container).data("active"))) {
 							$($element).addClass("jq-star-ranking-full").removeClass("jq-star-ranking-empty");
 						}
 						else {
